37bd0aa3fa64d5aa3324b6749f462be7c6a2fe66
MAURO GAY <MAURO.GAY@student.intecbrussel.be>
2020-11-27 10:54:23 +0100

    Add project files.

---
+++ 271120 Validator Kenan/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidatorProgram
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Is Integer Test: Geef een getal: ");
+            string input = Console.ReadLine();
+            Validator.isInteger(input);
+            Console.ReadLine();
+
+            Console.Write("Lege String test: Type iets of niets: ");
+            string input2 = Console.ReadLine();
+            Validator.IsNotEmpty(input2.ToString());
+            Console.ReadLine();
+
+            Console.Write("Test if NOT '0': Geef een getal: ");
+            string input3 = Console.ReadLine();
+            Validator.IsNotZero(input3);
+            Console.ReadLine();
+
+            Console.Write("Test if between 20 and 100: Geef een getal: ");
+            string input4 = Console.ReadLine();
+            Validator.IsBetweenMinAndMaxInt(input4);
+            Console.ReadLine();
+
+            Console.Write("Test if positive: Geef een getal: ");
+            string input5 = Console.ReadLine();
+            Validator.IsPositive(input5);
+            Console.ReadLine();
+
+            Console.Write("Test if negative: Geef een getal: ");
+            string input6 = Console.ReadLine();
+            Validator.IsNegative(input6);
+            Console.ReadLine();
+
+            Console.Write("Test if decimal and integer: Geef een getal: ");
+            string input7 = Console.ReadLine();
+            Validator.isNumber(input7);
+            Console.ReadLine();
+            Console.WriteLine($"Test if input {input7} is integer: ");
+            Validator.isInteger(input7);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ 271120 Validator Kenan/Validator.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidatorProgram
+{
+    static class Validator
+    {
+        public static bool IsNotZero(string value)
+        {
+            if (isInteger(value) == true)
+                if (int.Parse(value) != 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Waarde is 0. Error");
+                    return false;
+                }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool IsNotEmpty(string value)
+        {
+            if (value != "")
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Lege string. Error.");
+                return false;
+            }
+        }
+        public static bool IsBetweenMinAndMaxInt(string value)
+        {
+            int min = 20;
+            int max = 100;
+            if (isInteger(value) == true)
+                if (int.Parse(value) > min && int.Parse(value) < max)
+                {
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Waarde is niet binnen de range Min en Max. Error");
+                    return false;
+                }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool IsNegative(string value)
+        {
+            if (isInteger(value) == true)
+                if (int.Parse(value) < 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Waarde is niet negatief. Error");
+                    return false;
+                }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool IsPositive(string value)
+        {
+            if (isInteger(value) == true)
+                if (int.Parse(value) > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Waarde is niet positief. Error");
+                    return false;
+                }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool isInteger(string value)
+        {
+            if (isNumber(value) == true)
+            {
+                int number;
+                if (Int32.TryParse(value, out number))
+                {
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("No integer. Error.");
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public static bool isNumber(string value)
+        {
+            decimal number;
+            if (decimal.TryParse(value, out number))
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("No Valid Input. Error.");
+                return false;
+            }
+        }
+
+
+    }
+}
